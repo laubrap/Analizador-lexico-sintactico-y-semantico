42f018137a4045fb107135d6e360af2da4f7c276
FrancoTarsia <ftarsia@frba.utn.edu.ar>
2025-09-03 18:54:09 -0300

    correccion de gramaticas y agregado de archivos faltantes para la ejecucion del analizador lexico

--- TP3/src/general.c
+++ TP3/src/general.c
@@ -5,7 +5,30 @@
 
 #include "general.h"
 
+#include "structsTP3.h"
+
 extern YYLTYPE yylloc;
+
+int sumaDecimales = 0;
+int acumuladorIdentificadores = 0;
+int num_linea = 1;
+char* buffer = NULL;
+
+nodoIdentificadores *raizIdentificador = NULL;
+nodoLiteralCadena *raizLiterales = NULL;
+nodoDecimal *raizDecimal = NULL;
+nodoHexadecimal *raizHexadecimal = NULL;
+nodoOctal *raizOctal = NULL;
+nodoReservada *raizPalabraReservada = NULL;
+nodoReal *raizReal = NULL;
+nodoCaracter *raizCaracter = NULL;
+nodoPuntuaciones *raizPuntuaciones = NULL;
+nodoCadenasNoReconocidas* raizNoReconocida = NULL;
+
+int yylval = 0;
+
+int linea_actual = 1;
+int columna_actual = 1;
 
 void pausa(void)
 {
@@ -95,3 +118,4 @@ char* devolverAParser(char *aDevolver){
     if (strcmp(aDevolver, "{") == 0) return '{';
     if (strcmp(aDevolver, "}") == 0) return '}';
 }
+
---
+++ TP3/src/structsTP3.h
@@ -0,0 +1,122 @@
+#ifndef TIPOS_H
+#define TIPOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <errno.h>
+#include <limits.h>
+
+typedef enum {
+    clase_almacenamiento,
+    especificador_tipo,
+    clasificador_tipo,
+    struct_union,
+    enumeracion,
+    etiqueta,
+    seleccion,
+    iteracion,
+    salto,
+    unario
+}CategoriaReservada;
+
+typedef struct{
+    char *nombreIdentificador;
+    int contador;
+}infoNodoIdentificadores;
+typedef struct{
+    char *nombrePuntuaciones;
+    int contador;
+}infoNodoPuntuaciones;
+
+typedef struct{
+    char *nombreLiteralCadena;
+    int longitud; 
+}infoNodoLiteralCadena;
+
+typedef struct {
+    char* hexadecimal;
+    int decimal;
+} infoNodoHexadecimal;
+
+typedef struct {
+    char* octal;
+    int decimal;
+} infoNodoOctal;
+typedef struct {
+    char* palabra;
+    int linea;
+    int columna;
+    CategoriaReservada categoria;
+} infoNodoPalabrasReservada;
+
+typedef struct {
+    char* valor;
+    float parteEntera;
+    float mantisa;
+}infoNodoReal;
+
+typedef struct {
+    char* caracter;
+    struct nodoCaracter *sgte;
+}infoNodoCaracter;
+
+
+typedef struct{
+    int valor;
+    struct nodoDecimal *sgte;
+}nodoDecimal;
+
+typedef struct {
+    char* palabra;
+    int linea;
+    int columna;
+} infoNodoCadenasNoReconocidas;
+
+typedef struct {
+    infoNodoHexadecimal info;
+    struct nodoHexadecimal *sgte;
+}nodoHexadecimal;
+
+typedef struct {
+    infoNodoOctal info;
+    struct nodoOctal *sgte;
+}nodoOctal;
+
+
+typedef struct{
+    infoNodoReal info;
+    struct nodoReal *sgte;
+}nodoReal;
+
+typedef struct{
+    infoNodoCaracter info;
+    struct nodoCaracter *sgte;
+}nodoCaracter;
+
+typedef struct{
+    infoNodoIdentificadores info;
+    struct nodoIdentificadores *sgte;
+}nodoIdentificadores;
+
+typedef struct{
+    infoNodoLiteralCadena info;
+    struct nodoLiteralCadena *sgte;
+}nodoLiteralCadena;
+typedef struct{
+    infoNodoPalabrasReservada info;
+    struct nodoReservada* sgte;
+} nodoReservada;
+
+typedef struct{
+    infoNodoPuntuaciones info;
+   struct nodoPuntuaciones * sgte;
+} nodoPuntuaciones;
+
+typedef struct{
+    infoNodoCadenasNoReconocidas info;
+   struct nodoCadenasNoReconocidas * sgte;
+} nodoCadenasNoReconocidas;
+
+#endif /* TIPOS_H */
